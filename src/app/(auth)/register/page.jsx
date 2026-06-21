4bea163708dd5eac4cda7b94babe8ca8b97cfa60
Muhammad-Kashif-Soft-Eng <muhammad.kashif.soft.eng@gmail.com>
2026-06-21 23:07:18 +0500

    Backend live url is corrected.

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -12,7 +12,7 @@ import {
     FaEyeSlash,
 } from "react-icons/fa";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
+const API_URL = process.env.ECOMMERSE_BACKEND_URL || "http://localhost:5000/api";
 
 export default function RegisterPage() {
     const [showPassword, setShowPassword] = useState(false);
